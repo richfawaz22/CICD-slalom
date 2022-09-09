f4916cbc1b42a5d83794bb6f36850331c170d14e
Richard Fawaz <Richard.Fawaz@slalom.com>
2022-09-09 14:11:36 -0400

    failing unit test

--- main.tf
+++ main.tf
@@ -96,3 +96,17 @@ output "web-address" {
 
 
 }
+
+
+//messing around with docker hub but not working right now 
+# //provisioner "remote-exec" {
+#     inline = [
+#       "sudo yum update -y",
+#       "sudo amazon-linux-extras install docker -y",
+#       "sudo service docker start",
+#       "sudo usermod -a -G docker ec2-user",
+#       "sudo chkconfig docker on",
+#       "sudo yum install -y git",
+#       "sudo chmod 666 /var/run/docker.sock",
+#       "docker pull ",< image here >
+#       "docker run -d -p 80:80 "   
