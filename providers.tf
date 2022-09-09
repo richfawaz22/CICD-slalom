bd9fd28819d24e240862ca4a7284595c44288b5a
Richard Fawaz <Richard.Fawaz@slalom.com>
2022-09-09 00:25:58 -0400

    testing

--- providers.tf
+++ providers.tf
@@ -1,10 +1,7 @@
 terraform {
-  required_version = ">0.14.7"
-  backend "s3" {}
-
   required_providers {
     aws = {
-      version = ">= 3.66.0"
+      version = ">= 3.7.0"
       source  = "hashicorp/aws"
     }
   }
